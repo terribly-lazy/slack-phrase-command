283dddfac92750e77835e184f4d8959a81d82a9b
Kegan Myers <kegan@keganmyers.com>
2014-08-09 17:20:03 -0500

    Framework changes

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+var _ = require('lodash');
+
 module.exports = function(configuration) {
+    var listenOn = (function(name) {
+        switch (typeof name) {
+            case "string":
+                return [name];
+            case "object":
+                if (name instanceof Array) {
+                    return name;
+                }
+            //fallthrough
+            default:
+                return ["phrase"];
+        }
+    })(configuration.command);
+
+    function handler(slack) {
+        try {
+            slack.replyUser(slack.text);
+        } catch (e) {
+            slack.error(e.toString());
+        }
+    }
+
     return function(ee) {
-        ee.on('roll', function(slack) {
-            try {
-                slack.replyUser(slack.text);
-            } catch (e) {
-                slack.error(e.toString());
-            }
+        _.forEach(listenOn, function(commandName) {
+            ee.on(commandName, handler);
         });
     };
 };
