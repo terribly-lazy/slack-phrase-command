bf7fd428a68f4c43d4a3981a52b86a328c5fd7b1
Kegan Myers <kegan@keganmyers.com>
2014-08-09 17:49:22 -0500

    Allow better configuration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var _ = require('lodash');
+var fs = require('fs'),
+    _ = require('lodash');
 
 module.exports = function(configuration) {
     var listenOn = (function(name) {
@@ -14,10 +15,23 @@ module.exports = function(configuration) {
                 return ["phrase"];
         }
     })(configuration.command);
+    var phrases = configuration.phrases;
+    phrases = (typeof phrases === "object" && phrases instanceof Array) ? phrases : [];
+
+    if (typeof configuration.phraseFile === "string") {
+        phrases = _.compact(_.union(phrases, fs.readFileSync(configuration.phraseFile).split("\n")));
+    }
+
+    function phrase() {
+        if (phrases.length)
+            return phrases[Math.floor(Math.random() * phrases.length)];
+
+        return "This phrase command has not been properly configured."
+    }
 
     function handler(slack) {
         try {
-            slack.replyUser(slack.text);
+            slack.replyUser(phrase());
         } catch (e) {
             slack.error(e.toString());
         }
